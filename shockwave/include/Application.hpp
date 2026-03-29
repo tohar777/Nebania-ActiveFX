a1bcf42d9db7683d9e352989c95899b75803cacd
tohar777 <zionitoar452@gmail.com>
2026-03-29 14:06:02 +0300

    New:ShockwavePlayer

--- shockwave/include/Application.hpp
+++ shockwave/include/Application.hpp
@@ -6,16 +6,22 @@
 #include <SDL2/SDL_opengl.h>
 #include <SDL2/SDL_events.h>
 #include <GL/glu.h>
+#include <pugixml.hpp>
 #include <cstdlib>
 #include <ctime>
+#include "adl.hpp"
+
 class Shockwave{
     public:
         SDL_Window* window;
         SDL_GLContext gl;
         SDL_Renderer* renderer;
         SDL_Event e;
+        pugi::xml_document doc;
+        pugi::xml_node Application;
+        ADLHandler adl;
         bool isRunning = true;
-        void InitWindow(int w,int h);
+        void InitWindow();
         void Events();
         void Reshape();
         void Display();
